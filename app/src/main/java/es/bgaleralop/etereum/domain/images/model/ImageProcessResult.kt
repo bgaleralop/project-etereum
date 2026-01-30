1e063956fac1de43e581085490113e4e73626da0
bgaleradev <bgaleradev@gmail.com>
2026-01-30 18:42:32 +0100

    Cambiado tipo de datos del parametro image en ImageProcessResult.kt para hacerlo extensible a otras plataformas

--- app/src/main/java/es/bgaleralop/etereum/domain/images/model/ImageProcessResult.kt
+++ app/src/main/java/es/bgaleralop/etereum/domain/images/model/ImageProcessResult.kt
@@ -6,7 +6,7 @@ import android.graphics.Bitmap
  * Clase de datos que representa una imagen procesada
  */
 data class ImageProcessResult(
-    val image: Bitmap,
+    val image: ByteArray,
     val weightInKb: Int,
     val isSanitized: Boolean = false
 )
